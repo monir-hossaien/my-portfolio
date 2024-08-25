70fb8e77a913605b4c4750f1261b4ba5725a1930
Monir Hossain <monirhdigital@gmail.com>
2024-08-25 14:53:27 +0600

    version 1.0.1

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+// -------------about----------
+
+const tabLinks = document.getElementsByClassName("tab-links");
+const tabContents = document.getElementsByClassName("tab-contents");
+
+function openTab(tabName){
+    for(tabLink of tabLinks){
+        tabLink.classList.remove("active-link");
+    }
+
+    for(tabContent of tabContents){
+        tabContent.classList.remove("active-tab");
+    }
+    
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabName).classList.add("active-tab");
+}
