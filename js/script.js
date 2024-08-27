7c88339e10ffafadb9ce1ea8ea72a3ed65b15dce
Monir Hossain <monirhdigital@gmail.com>
2024-08-27 17:21:58 +0600

    version 1.0.4

--- js/script.js
+++ js/script.js
@@ -16,3 +16,28 @@ function openTab(tabName){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabName).classList.add("active-tab");
 }
+
+
+//contact form
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyg2NbT1yA1YJY2g1jc8gPIIYX2ynAdXXSZs0V88Mu1_O5xkCVhUkoQik5a6gwJfNMb/exec'
+const form = document.forms['submit-to-google-sheet']
+const message = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response =>{
+        message.innerHTML="Message Sent Successfully";
+        message.classList.add("msg-style");
+
+        setTimeout(()=>{
+            message.innerHTML="";
+            message.classList.remove("msg-style");
+        }, 1000);
+        
+        form.reset();
+    })
+    .catch(error => console.error('Error!', error.message))
+    
+})
